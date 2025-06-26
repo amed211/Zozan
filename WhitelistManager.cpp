000e9a65ba521046af987c96141123a2f692c10b
amed211 <132192659+amed211@users.noreply.github.com>
2025-06-26 21:44:46 +0300

    Delete WhitelistManager.cpp

--- WhitelistManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "WhitelistManager.h"
-
-bool WhitelistManager::isWhitelisted(int pid) {
-    return whitelist.find(pid) != whitelist.end();
-}
-
-void WhitelistManager::addToWhitelist(int pid) {
-    whitelist.insert(pid);
-}
